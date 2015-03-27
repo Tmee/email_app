f49aeadf1a6e5be5d6d6d34ca9cfde60827e4d57
Timothy Mee <tjmee90@gmail.com>
2015-03-26 21:43:23 -0600

    removed unused code

--- app/models/not.rb
+++
@@ -1,2 +0,0 @@
-class Not < ActiveRecord::Base
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it "send an email to another user" do
     user = User.create(name: 'tim', email: 'example2@exmaple.com', password: 'password')
-    puts "#{user.name} ::::: #{user.email}"
     user.send_email("tjmee90@gmail.com", "This is a testing thing", "and the body goes here")
     result = ActionMailer::Base.deliveries.last
 
