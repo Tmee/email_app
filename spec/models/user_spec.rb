96f194e9fa5591db4c37a3ef6a744b5622d69075
Tim M. <tjmee90@gmail.com>
2015-03-27 13:15:23 -0600

    spelling

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "send an email to another user" do
+  it "sends an email to another user" do
     user = User.create(name: 'tim', email: 'example2@exmaple.com', password: 'password')
     user.send_email("simple@example.com", "This is a testing thing", "and the body goes here")
     result = ActionMailer::Base.deliveries.last
